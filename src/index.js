835823a7bf83a08c209114f97e2c4fd5eaea3657
BilalIsaKhan <bilalisakhan97@gmail.com>
2022-11-26 18:40:03 +0500

    GraphQL API Creation Lesson

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
-import myAddFunction, { subtractFunction } from "./math";
+import { GraphQLServer } from "graphql-yoga";
 
-console.log(myAddFunction(23, 34));
-console.log(subtractFunction(34, 23));
+//Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        Bio: String!
+    }
+`;
+
+//Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query!";
+    },
+    name() {
+      return "Bilal";
+    },
+    location() {
+      return "Islamabad";
+    },
+    Bio() {
+      return "I am a developer and learning GraphQL at the momemt";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("The server is up!");
+});
