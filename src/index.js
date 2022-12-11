71ded719d7aeb663aa399a15a3890f4aab6baf69
BilalIsaKhan <bilalisakhan97@gmail.com>
2022-12-11 17:57:45 +0500

    Mutation lesson start commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
-
+import uuidv4 from "uuid/v4";
 //Demo User Data
 const users = [
   {
@@ -80,6 +80,11 @@ const typeDefs = `
       me: User!
       post: Post!
     }
+    type Mutation {
+      createUser(name: String!, email: String!, age: Int): User!
+      createPost(title: String!, body: String!, published: Boolean!, author:ID! ): Post!
+      createComment(text: String!, author: ID!, post: ID!): Comment!
+    }
     type User{
       id:ID!
       name: String!
@@ -141,6 +146,59 @@ const resolvers = {
         body: "Monday generally sucks, doesnt have anything to do with this",
         published: false,
       };
+    },
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some((user) => user.email === args.email);
+      if (emailTaken) {
+        throw new Error("Email taken.");
+      }
+
+      const user = {
+        id: uuidv4(),
+        name: args.name,
+        email: args.email,
+        age: args.age,
+      };
+
+      users.push(user);
+      return user;
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author);
+      if (!userExists) {
+        throw new Error("User not found");
+      }
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author,
+      };
+      posts.push(post);
+      return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author);
+      if (!userExists) {
+        throw new Error("User not found");
+      }
+      const postExists = posts.some(
+        (post) => post.id === args.post && post.published
+      );
+      if (!postExists) {
+        throw new Error("Post does not exist");
+      }
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post,
+      };
+      comments.push(comment);
+      return comment;
     },
   },
   Post: {
